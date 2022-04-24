efa8ccad09ddb751077948f784baf7dbf91254f6
hiroyuki573 <94595606+hiroyuki573@users.noreply.github.com>
2022-04-24 15:54:53 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+  //ハンバーガーメニュー
 {
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
@@ -13,5 +15,31 @@
   close.addEventListener('click', () =>{
     overlay.classList.remove('show');
     open.classList.remove('hide');
+  });
+}
+
+
+  //ふわっと移動
+{
+  function callback(entries,obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
   });
 }
